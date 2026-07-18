342887ecfd875f6caa8cc14eee5dfa472955452b
GreeshmaSai2006 <Vakkalagaddagreeshma2006@gmail.com>
2026-07-18 10:24:27 +0530

    Delete String.js

--- String.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-let firstName = "John";
-let lastName = "Doe";
-console.log("Full Name:", firstName + " " + lastName);
-console.log("Uppercase:", firstName.toUpperCase());
-console.log("Lowercase:", lastName.toLowerCase());
-console.log("Length:", firstName.length);
