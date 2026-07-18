56b5f4b468aa08d489eb4a2d3409b22b12ac2733
GreeshmaSai2006 <Vakkalagaddagreeshma2006@gmail.com>
2026-07-18 10:20:33 +0530

    Delete Boolean.ts

--- Boolean.ts
+++
@@ -1,9 +0,0 @@
-let age: number = 20;
-let isAdult: boolean = age >= 18;
-let isStudent: boolean = true;
-
-console.log("Is Adult:", isAdult);
-console.log("Is Student:", isStudent);
-
-console.log("Both conditions:", isAdult && isStudent);
-console.log("Either condition:", isAdult || isStudent);
