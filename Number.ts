b96e3197768e313a4d59084be39b29c80276e9a1
GreeshmaSai2006 <Vakkalagaddagreeshma2006@gmail.com>
2026-08-08 18:07:23 +0530

    Delete Number.ts

--- Number.ts
+++
@@ -1,8 +0,0 @@
-let a: number = 10;
-let b: number = 5;
-
-console.log("Addition:", a + b);
-console.log("Subtraction:", a - b);
-console.log("Multiplication:", a * b);
-console.log("Division:", a / b);
-console.log("Modulus:", a % b);
