6d952cb7954d238e8dc8c2b675622fcbe85733ec
GreeshmaSai2006 <Vakkalagaddagreeshma2006@gmail.com>
2026-09-19 10:23:39 +0530

    Create Student.js

---
+++ week10/App3/Student.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Course from "./Course"; // Importing the course component
+
+function Student() {
+  return (
+    <div>
+      <h2>Student Details</h2>
+      {/* Nesting the Course component inside Student */}
+      <Course /> 
+    </div>
+  );
+}
+
+export default Student;
