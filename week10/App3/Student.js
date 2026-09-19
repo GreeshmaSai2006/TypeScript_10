019180b149e68fdb1562e9d2a518bcf20ae7fc30
GreeshmaSai2006 <Vakkalagaddagreeshma2006@gmail.com>
2026-09-19 10:29:58 +0530

    Update Student.js

--- week10/App3/Student.js
+++ week10/App3/Student.js
@@ -5,6 +5,7 @@ function Student() {
   return (
     <div>
       <h2>Student Details</h2>
+      <p> Name:Greeshma </p>
       {/* Nesting the Course component inside Student */}
       <Course /> 
     </div>
